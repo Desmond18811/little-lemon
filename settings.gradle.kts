38c28c37478d78c012aaa0ca6765ba3c4dfc2518
Desmond18811 <ubidesmond31@gmail.com>
2024-10-17 16:14:01 +0100

    Add files via upload

---
+++ settings.gradle.kts
@@ -0,0 +1,21 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "LittleLemonMenuDishes"
+include(":app")
+rootProject.name = "LittleLemonMenuDishes"
+include(":app")
+ 
